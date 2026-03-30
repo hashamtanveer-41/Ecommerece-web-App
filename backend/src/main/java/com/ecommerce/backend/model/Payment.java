610dd378a490ae16216734dee44255a3080e52c0
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-03-30 18:12:27 +0500

    implement payment and order management; add Payment and Order entities with relationships, validation, and repository integration

--- backend/src/main/java/com/ecommerce/backend/model/Payment.java
+++ backend/src/main/java/com/ecommerce/backend/model/Payment.java
@@ -1,4 +1,33 @@
 package com.ecommerce.backend.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "payments")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+
+    @OneToOne(mappedBy = "payment", cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    private Order order;
+
+    @NotBlank
+    @Size(min = 3, message = "Payment method must contain atleast 3 characters.")
+    private String paymentMethod;
+
+    private String pgPaymentId;
+    private String pgStatus;
+    private String pgResponseMessage;
+    private String pgName;
+
 }
--- backend/src/main/java/com/ecommerce/backend/repostories/OrderRepository.java
+++ backend/src/main/java/com/ecommerce/backend/repostories/OrderRepository.java
@@ -1,4 +1,8 @@
 package com.ecommerce.backend.repostories;
 
-public class OrderRepository {
+import com.ecommerce.backend.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
 }
